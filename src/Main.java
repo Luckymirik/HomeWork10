94ae209739571612b4fd4c9abc95c231edc60fbc
LuckyMirik <Luckymirik@mail.ru>
2023-05-05 10:46:40 +0300

    Сделал ДЗ

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,44 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        task1();
+        task2();
+        task3();
+
+
+
+    }
+
+    public static void task1() {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Задача 1");
         String firstName = scanner.next();
         String middleName = scanner.next();
         String lastName = scanner.next();
-        String fullName = firstName+" "+middleName+" "+lastName;
-        System.out.println("ФИО сотрудника—" + fullName);
+        String fullName = firstName + " " + middleName + " " + lastName;
 
+        System.out.println("ФИО сотрудника — " + fullName);
+    }
+
+    public static void task2() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Задача 2");
+        String firstName = scanner.next();
+        String middleName = scanner.next();
+        String lastName = scanner.next();
+        String fullName = firstName + " " + middleName + " " + lastName;
+        fullName = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName);
+    }
+    public static void task3() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Задача 3");
+        String firstName = scanner.next();
+        String middleName = scanner.next();
+        String lastName = scanner.next();
+        String fullName = firstName + " " + middleName + " " + lastName;
+        fullName = fullName.replace("ё","е");
+        System.out.println("Данные ФИО сотрудника — " + fullName);
     }
 }
